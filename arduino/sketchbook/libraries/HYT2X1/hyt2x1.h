2d11c00323d07ee52ee72e7f765c7a4aaa054e97
Marco Baldinetti <m.baldinetti@digiteco.it>
2017-10-11 18:18:46 +0200

    add small doc to HYT2X1 library

--- arduino/sketchbook/libraries/HYT2X1/hyt2x1.h
+++ arduino/sketchbook/libraries/HYT2X1/hyt2x1.h
@@ -1,4 +1,6 @@
-/**********************************************************************
+/**@file hyt2x1.h */
+
+/*********************************************************************
 Copyright (C) 2017  Marco Baldinetti <m.baldinetti@digiteco.it>
 authors:
 Marco Baldinetti <m.baldinetti@digiteco.it>
