7b4186ef5d2f0ae1c7964eb8b63956c5ec069992
Marco Baldinetti <m.baldinetti@digiteco.it>
2018-06-14 15:12:04 +0200

    fix init function

--- arduino/sketchbook/libraries/HYT2X1/hyt2x1.cpp
+++ arduino/sketchbook/libraries/HYT2X1/hyt2x1.cpp
@@ -22,63 +22,65 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include "hyt2x1.h"
 
 namespace Hyt2X1 {
+  bool hyt_initRead(uint8_t address) {
+    Wire.beginTransmission(address);
+    if (Wire.endTransmission()) {
+      return false;
+    }
+    return true;
+  }
 
-   uint32_t hyt_initRead(uint8_t address) {
-      Wire.beginTransmission(address);
-      return HYT2X1_CONVERSION_TIME_MS;
-   }
+  bool hyt_read(int8_t address, float *humidity, float *temperature) {
+    unsigned long raw_data = 0xFFFF;
 
-   bool hyt_read(int8_t address, float *humidity, float *temperature) {
-      unsigned long raw_data = 0xFFFF;
+    //! Request 4 bytes: 2 bytes for Humidity and 2 bytes for Temperature
+    Wire.requestFrom(address, HYT2X1_READ_HT_DATA_LENGTH);
 
-      //! Request 4 bytes: 2 bytes for Humidity and 2 bytes for Temperature
-      Wire.requestFrom(address, HYT2X1_READ_HT_DATA_LENGTH);
+    if (Wire.available() < HYT2X1_READ_HT_DATA_LENGTH) {
+      return false;
+    }
 
-      if (Wire.available() < HYT2X1_READ_HT_DATA_LENGTH) {
-         return false;
-      }
+    //! read 4 bytes of raw data
+    raw_data = (unsigned long) Wire.read() << 24 | (unsigned long) Wire.read() << 16 | (unsigned long) Wire.read() << 8 | (unsigned long) Wire.read();
 
-      //! read 4 bytes of raw data
-      raw_data = (unsigned long) Wire.read() << 24 | (unsigned long) Wire.read() << 16 | (unsigned long) Wire.read() << 8 | (unsigned long) Wire.read();
+    //! extract 14 bit humidity right adjusted (bit 0-14)
+    *humidity = 100.0 / 0x3FFF * (raw_data >> 16 & 0x3FFF);
 
-      //! extract 14 bit humidity right adjusted (bit 0-14)
-      *humidity = 100.0 / 0x3FFF * (raw_data >> 16 & 0x3FFF);
+    //! extract 14 bit temperature left adjusted (bit 2-16)
+    *temperature = 165.0 / 0x3FFF * (((unsigned int) raw_data) >> 2) - 40;
 
-      //! extract 14 bit temperature left adjusted (bit 2-16)
-      *temperature = 165.0 / 0x3FFF * (((unsigned int) raw_data) >> 2) - 40;
+    // if (Wire.endTransmission()) {
+    //   return false;
+    // }
 
-      if (Wire.endTransmission()) {
-         return false;
-      }
+    return true;
+  }
 
-      return true;
-   }
+  void hyt_send(int8_t address, uint8_t data_0, uint8_t data_1, uint8_t data_2) {
+    Wire.beginTransmission(address);
+    Wire.write(data_0);
+    Wire.write(data_1);
+    Wire.write(data_2);
+    Wire.endTransmission();
+  }
 
-   void hyt_send(int8_t address, uint8_t data_0, uint8_t data_1, uint8_t data_2) {
-      Wire.beginTransmission(address);
-      Wire.write(data_0);
-      Wire.write(data_1);
-      Wire.write(data_2);
-      Wire.endTransmission();
-   }
+  void hyt_changeAddress(uint8_t power_pin, int8_t address, int8_t new_address) {
+    hyt_off(power_pin);
+    hyt_on(power_pin);
+    hyt_send(address, HYT2X1_ENTER_COMMAND_MODE, 0x00, 0x00);
+    hyt_send(address, HYT2X1_WRITE_ADDRESS, 0x00, new_address);
+    hyt_send(address, HYT2X1_EXIT_COMMAND_MODE, 0x00, 0x00);
+  }
 
-   void hyt_changeAddress(uint8_t power_pin, int8_t address, int8_t new_address) {
-      hyt_off(power_pin);
-      hyt_on(power_pin);
-      hyt_send(address, HYT2X1_ENTER_COMMAND_MODE, 0x00, 0x00);
-      hyt_send(address, HYT2X1_WRITE_ADDRESS, 0x00, new_address);
-      hyt_send(address, HYT2X1_EXIT_COMMAND_MODE, 0x00, 0x00);
-   }
+  void hyt_init(uint8_t power_pin) {
+    pinMode(power_pin, OUTPUT);
+  }
 
-   void hyt_init(uint8_t power_pin) {
-      pinMode(power_pin, OUTPUT);
-   }
+  void hyt_on(uint8_t power_pin) {
+    digitalWrite(power_pin, HIGH);
+  }
 
-   void hyt_on(uint8_t power_pin) {
-      digitalWrite(power_pin, HIGH);
-   }
-
-   void hyt_off(uint8_t power_pin) {
-      digitalWrite(power_pin, LOW);
-   }
+  void hyt_off(uint8_t power_pin) {
+    digitalWrite(power_pin, LOW);
+  }
 }
--- arduino/sketchbook/libraries/HYT2X1/hyt2x1.h
+++ arduino/sketchbook/libraries/HYT2X1/hyt2x1.h
@@ -90,68 +90,68 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 \brief HYT2X1 namespace.
 */
 namespace Hyt2X1 {
-   /*!
-   \fn void hyt_init(uint8_t power_pin)
-   \brief Init sensor.
-   \param[in] power_pin sensors power pin.
-   \return void.
-   */
-   void hyt_init(uint8_t power_pin);
+  /*!
+  \fn void hyt_init(uint8_t power_pin)
+  \brief Init sensor.
+  \param[in] power_pin sensors power pin.
+  \return void.
+  */
+  void hyt_init(uint8_t power_pin);
 
-   /*!
-   \fn void hyt_on(uint8_t power_pin)
-   \brief Power on sensor.
-   \param[in] power_pin sensors power pin.
-   \return void.
-   */
-   void hyt_on(uint8_t power_pin);
+  /*!
+  \fn void hyt_on(uint8_t power_pin)
+  \brief Power on sensor.
+  \param[in] power_pin sensors power pin.
+  \return void.
+  */
+  void hyt_on(uint8_t power_pin);
 
-   /*!
-   \fn void hyt_off(uint8_t power_pin)
-   \brief Power off sensor.
-   \param[in] power_pin sensors power pin.
-   \return void.
-   */
-   void hyt_off(uint8_t power_pin);
+  /*!
+  \fn void hyt_off(uint8_t power_pin)
+  \brief Power off sensor.
+  \param[in] power_pin sensors power pin.
+  \return void.
+  */
+  void hyt_off(uint8_t power_pin);
 
-   /*!
-   \fn void hyt_changeAddress(uint8_t power_pin, int8_t address, int8_t new_address)
-   \brief Change sensor address.
-   \param[in] power_pin sensors power pin.
-   \param[in] address sensors i2c address.
-   \param[in] new_address sensors i2c new address.
-   \return void.
-   */
-   void hyt_changeAddress(uint8_t power_pin, int8_t address, int8_t new_address);
+  /*!
+  \fn void hyt_changeAddress(uint8_t power_pin, int8_t address, int8_t new_address)
+  \brief Change sensor address.
+  \param[in] power_pin sensors power pin.
+  \param[in] address sensors i2c address.
+  \param[in] new_address sensors i2c new address.
+  \return void.
+  */
+  void hyt_changeAddress(uint8_t power_pin, int8_t address, int8_t new_address);
 
-   /*!
-   \fn uint32_t hyt_initRead(uint8_t address)
-   \brief Init sensor read.
-   \param[in] address sensors i2c address.
-   \return uint32_t sensors conversion time.
-   */
-   uint32_t hyt_initRead(uint8_t address);
+  /*!
+  \fn bool hyt_initRead(uint8_t address)
+  \brief Init sensor read.
+  \param[in] address sensors i2c address.
+  \return true if success.
+  */
+  bool hyt_initRead(uint8_t address);
 
-   /*!
-   \fn bool hyt_read(int8_t address, float *humidity, float *temperature)
-   \brief Returns the humidty and temperature from hyt2X1 sensor at specified address.
-   \param[in] address sensor i2c address.
-   \param[out] *humidity pointer to readed humidity variable.
-   \param[out] *temperature pointer to readed temperature variable.
-   \return true if success.
-   */
-   bool hyt_read(int8_t address, float *humidity, float *temperature);
+  /*!
+  \fn bool hyt_read(int8_t address, float *humidity, float *temperature)
+  \brief Returns the humidty and temperature from hyt2X1 sensor at specified address.
+  \param[in] address sensor i2c address.
+  \param[out] *humidity pointer to readed humidity variable.
+  \param[out] *temperature pointer to readed temperature variable.
+  \return true if success.
+  */
+  bool hyt_read(int8_t address, float *humidity, float *temperature);
 
-   /*!
-   \fn void hyt_send(int8_t address, uint8_t data_0, uint8_t data_1, uint8_t data_2)
-   \brief Send sensor command.
-   \param[in] address sensor i2c address.
-   \param[in] data_0 first byte data.
-   \param[in] data_1 second byte data.
-   \param[in] data_2 third byte data.
-   \return void.
-   */
-   void hyt_send(int8_t address, uint8_t data_0, uint8_t data_1, uint8_t data_2);
+  /*!
+  \fn void hyt_send(int8_t address, uint8_t data_0, uint8_t data_1, uint8_t data_2)
+  \brief Send sensor command.
+  \param[in] address sensor i2c address.
+  \param[in] data_0 first byte data.
+  \param[in] data_1 second byte data.
+  \param[in] data_2 third byte data.
+  \return void.
+  */
+  void hyt_send(int8_t address, uint8_t data_0, uint8_t data_1, uint8_t data_2);
 };
 
 #endif
